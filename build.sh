5d8ac97bb035f43e58b6f2c7428e312059e53a3b
Christian Kvalheim <christkv@gmail.com>
2013-01-31 17:40:56 +0100

    Just fixed publish script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ make html
 cp -R _build/html/ ~/coding/checkout/learn-node-mongo-the-hard-way/.
 cd ~/coding/checkout/learn-node-mongo-the-hard-way
 git commit -a -m "Updated book"
-git push
+git push gh-pages
 cd ~/coding/projects/learn-node-mongo-the-hard-way
