64ace4f86860b99c84b6588653279e07ffc1ca10
Christian Kvalheim <christkv@gmail.com>
2013-01-31 17:35:00 +0100

    Just silly publish script

---
+++ build.sh
@@ -0,0 +1,6 @@
+make html
+cp -R _build/html/ ~/coding/checkout/learn-node-mongo-the-hard-way/.
+cd ~/coding/checkout/learn-node-mongo-the-hard-way
+git commit -a -m "Updated book"
+git push
+cd ~/coding/projects/learn-node-mongo-the-hard-way
