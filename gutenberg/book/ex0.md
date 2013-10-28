bc37ab7ed6c62bafd9308d4fdff9080d836a72ec
Christian Kvalheim <christkv@gmail.com>
2013-10-28 15:53:38 +0100

    Selective regenerating of book chapters

--- gutenberg/book/ex0.md
+++ gutenberg/book/ex0.md
@@ -1,2 +1,4 @@
 Exercise 0: The Setup
 =====================
+
+Welcome to this change in scenario
--- gutenberg/src/main.go
+++ gutenberg/src/main.go
@@ -26,8 +26,19 @@ var (
 )
 
 type Process struct {
-	Done   chan bool
+	// Process is done
+	Done chan bool
+	// Source directory for the book
 	Source string
+	// Started
+	Started bool
+
+	// Contains the statistics for all the pages
+	ConfigFileInfo     *os.FileInfo
+	PageLayoutFileInfo *os.FileInfo
+	IndexFileInfo      *os.FileInfo
+	PagesFileInfo      map[string]*os.FileInfo
+	AssetsFileInfo     map[string]*os.FileInfo
 }
 
 func usage() {
@@ -52,14 +63,194 @@ func main() {
 	}
 
 	// Create a Process
-	process := &Process{Done: make(chan bool), Source: *source}
+	process := &Process{Done: make(chan bool),
+		Source:         *source,
+		Started:        true,
+		PagesFileInfo:  make(map[string]*os.FileInfo),
+		AssetsFileInfo: make(map[string]*os.FileInfo),
+	}
 
 	// Create output directory if it does not exist
 	err = os.Mkdir(c.OutputDirectory, 0755)
+
+	// Generate whole book
+	GenerateWholeBook(process)
+
 	// Go into watch mode
 	if *watchMode {
 		WatchMode(*interval, process)
 	}
+}
+
+func WatchMode(delay int64, p *Process) {
+	go func() {
+		for true {
+			// Get the parts of the config file
+			sourcePath := config.SourcePath(source)
+			configFileLocation := config.ConfigFile(sourcePath, cfgfile)
+
+			// Set the config file name
+			configFileInfo, err := os.Stat(configFileLocation)
+			if err != nil {
+				log.Printf("Failed to read configuration file from %s\n", configFileLocation)
+				continue
+			}
+
+			// Read the configuration
+			c, err := config.ReadConfigFromFile(cfgfile, source)
+			if err != nil {
+				log.Printf("Failed to read configuration file from %s\n", configFileLocation)
+				continue
+			}
+
+			// Point to the actual instance
+			configFileInfoInstance := *p.ConfigFileInfo
+
+			// If config time finished
+			if !configFileInfo.ModTime().Equal(configFileInfoInstance.ModTime()) {
+				log.Printf("Configuration file changed, regenerating whole book\n")
+				// Re-generate the whole book
+				GenerateWholeBook(p)
+				// Save the new configuration file info
+				p.ConfigFileInfo = &configFileInfo
+				// Done, let's watch again
+				continue
+			}
+
+			// Read all the layouts for html
+			htmlLayouts := c.Layouts["html"]
+			indexLayout := htmlLayouts.Index
+			pageLayout := htmlLayouts.Page
+
+			// Get the file info for the pageLayout file
+			pageLayoutFile := fmt.Sprintf("%s/%s", p.Source, pageLayout)
+			pageLayoutFileInfo, err := os.Stat(pageLayoutFile)
+			if err != nil {
+				log.Printf("Index layout file changed, regenerating whole book\n")
+				// Re-generate the whole book
+				GenerateWholeBook(p)
+				// Update the time
+				p.PageLayoutFileInfo = &pageLayoutFileInfo
+				continue
+			}
+
+			// Get the file info for the pageLayout file
+			indexLayoutFile := fmt.Sprintf("%s/%s", p.Source, indexLayout)
+			indexLayoutFileInfo, err := os.Stat(indexLayoutFile)
+			if err != nil {
+				log.Printf("Index layout file changed, regenerating whole book\n")
+				// Re-generate the whole book
+				GenerateWholeBook(p)
+				// Update the time
+				p.IndexFileInfo = &indexLayoutFileInfo
+				continue
+			}
+
+			// Read the page layout file in
+			layoutBytes, err := ioutil.ReadFile(pageLayoutFile)
+			if err != nil {
+				log.Printf("no layout file found for %s\n", pageLayoutFile)
+			}
+
+			// Parse the template into an object
+			pageTemplate, err := template.New("pageTemplate").Parse(string(layoutBytes))
+			if err != nil {
+				log.Printf("invalid template found in %s page template file\n", pageLayoutFile)
+			}
+
+			// Copy over all the assets
+			for _, asset := range c.Assets {
+				assetLocation := fmt.Sprintf("%s/%s", p.Source, asset)
+				fileContent, err := ioutil.ReadFile(assetLocation)
+
+				// Get the file info for the pageLayout file
+				assetFileInfo, err := os.Stat(assetLocation)
+				if err == nil {
+					modTime := *p.AssetsFileInfo[asset]
+
+					if modTime != nil && modTime.ModTime().Equal(assetFileInfo.ModTime()) {
+						continue
+					}
+
+					p.AssetsFileInfo[asset] = &assetFileInfo
+				}
+
+				if err == nil {
+					assetOutputLocation := fmt.Sprintf("%s/%s", c.OutputDirectory, filepath.Base(asset))
+					log.Printf("Saving asset to %s\n", assetOutputLocation)
+
+					err = ioutil.WriteFile(assetOutputLocation, fileContent, 0755)
+					if err != nil {
+						log.Fatalf("Failed to save the asset %s to %s\n", asset, assetOutputLocation)
+					}
+				} else {
+					log.Fatalf("Could not locate the asset %s\n", asset)
+				}
+			}
+
+			// We only re-generate pages that have changed
+			// Read all the pages in
+			for _, page := range c.TableOfContents {
+				// Split the file up so we can get the "name"
+				fileNameParts := strings.Split(page.File, ".")
+				fileName := strings.Join(fileNameParts[0:(len(fileNameParts)-1)], ".")
+
+				// Read the page
+				pageFile := fmt.Sprintf("%s/%s", p.Source, page.File)
+
+				// Get the file info for the pageLayout file
+				pageFileInfo, err := os.Stat(pageFile)
+				if err == nil {
+					modTime := *p.PagesFileInfo[page.File]
+
+					if modTime != nil && modTime.ModTime().Equal(pageFileInfo.ModTime()) {
+						continue
+					}
+
+					log.Printf("Generate page %s\n", page.File)
+					p.PagesFileInfo[page.File] = &pageFileInfo
+				}
+
+				// Read the page into memory
+				data, err := ioutil.ReadFile(pageFile)
+				if err != nil {
+					continue
+				}
+
+				// Get the custom Html transformer
+				customTransformer := gutenberg.NewCustomHtml(c)
+
+				// Render the mardown
+				html := customTransformer.Transform(data)
+
+				// Pass to the template if it's defined
+				if pageTemplate != nil {
+					// var buffer bytes.Buffer
+					buffer := bytes.NewBuffer(nil)
+					err = pageTemplate.Execute(buffer, BuildContext(string(html), c))
+					if err != nil {
+						log.Fatalf("Failed to execute template %s\n", pageLayoutFile)
+						os.Exit(0)
+					}
+
+					// Save the data as the new page
+					html = buffer.Bytes()
+				}
+
+				// Let's write the resulting page out
+				err = ioutil.WriteFile(fmt.Sprintf("%s/%s.html", c.OutputDirectory, fileName), html, 0755)
+				if err != nil {
+					continue
+				}
+			}
+
+			// Just sleep a bit and watch again
+			time.Sleep(time.Duration(delay) * time.Millisecond)
+		}
+	}()
+
+	// Wait until we are done
+	<-p.Done
 }
 
 type Page struct {
@@ -85,10 +276,34 @@ func BuildContext(html string, c *config.Config) map[string]interface{} {
 func GenerateBook(p *Process, c *config.Config) error {
 	// Read all the layouts for html
 	htmlLayouts := c.Layouts["html"]
+	indexLayout := htmlLayouts.Index
 	pageLayout := htmlLayouts.Page
+
+	// Get the parts of the config file
+	sourcePath := config.SourcePath(source)
+	// Set the config file name
+	configFile := config.ConfigFile(sourcePath, cfgfile)
+	configFileInfo, err := os.Stat(configFile)
+	if err == nil {
+		p.ConfigFileInfo = &configFileInfo
+	}
+
+	// Get the right location for the page layout file
+	indexLayoutFile := fmt.Sprintf("%s/%s", p.Source, indexLayout)
+	// Get the file info for the pageLayout file
+	indexLayoutFileInfo, err := os.Stat(indexLayoutFile)
+	if err == nil {
+		p.IndexFileInfo = &indexLayoutFileInfo
+	}
 
 	// Get the right location for the page layout file
 	pageLayoutFile := fmt.Sprintf("%s/%s", p.Source, pageLayout)
+	// Get the file info for the pageLayout file
+	pageLayoutFileInfo, err := os.Stat(pageLayoutFile)
+	if err == nil {
+		p.PageLayoutFileInfo = &pageLayoutFileInfo
+	}
+
 	// Read the page layout file in
 	layoutBytes, err := ioutil.ReadFile(pageLayoutFile)
 	if err != nil {
@@ -105,6 +320,12 @@ func GenerateBook(p *Process, c *config.Config) error {
 	for _, asset := range c.Assets {
 		assetLocation := fmt.Sprintf("%s/%s", p.Source, asset)
 		fileContent, err := ioutil.ReadFile(assetLocation)
+
+		// Get the file info for the pageLayout file
+		assetFileInfo, err := os.Stat(assetLocation)
+		if err == nil {
+			p.AssetsFileInfo[asset] = &assetFileInfo
+		}
 
 		if err == nil {
 			assetOutputLocation := fmt.Sprintf("%s/%s", c.OutputDirectory, filepath.Base(asset))
@@ -129,11 +350,15 @@ func GenerateBook(p *Process, c *config.Config) error {
 
 		// Read the page
 		pageFile := fmt.Sprintf("%s/%s", p.Source, page.File)
-		// Get the File info
-		// fileInfo, err := os.Stat(pageFile)
-		_, err = os.Stat(pageFile)
+
+		// Get the file info for the pageLayout file
+		pageFileInfo, err := os.Stat(pageFile)
 		if err != nil {
 			return err
+		}
+
+		if err == nil {
+			p.PagesFileInfo[page.File] = &pageFileInfo
 		}
 
 		// Read the page into memory
@@ -172,45 +397,75 @@ func GenerateBook(p *Process, c *config.Config) error {
 	return nil
 }
 
-func WatchMode(delay int64, p *Process) error {
-	go func() {
-		for true {
-			// Get the parts of the config file
-			sourcePath := config.SourcePath(source)
-			// Set the config file name
-			configFile := config.ConfigFile(sourcePath, cfgfile)
-			// Log the attempt to read the configuration file
-			log.Printf("Reading configuration file %s\n", configFile)
-			// Read the configuration
-			c, err := config.ReadConfigFromFile(cfgfile, source)
-			if err != nil {
-				fmt.Printf("Error:: %v\n", err)
-				p.Done <- true
-				break
-			}
+func GenerateWholeBook(p *Process) error {
+	// Get the parts of the config file
+	sourcePath := config.SourcePath(source)
+	// Set the config file name
+	configFile := config.ConfigFile(sourcePath, cfgfile)
+	// Log the attempt to read the configuration file
+	log.Printf("Reading configuration file %s\n", configFile)
+	// Read the configuration
+	c, err := config.ReadConfigFromFile(cfgfile, source)
+	if err != nil {
+		fmt.Printf("Error:: %v\n", err)
+		p.Done <- true
+		return err
+	}
 
-			// Set the source path
-			c.SourcePath = sourcePath
+	// Set the source path
+	c.SourcePath = sourcePath
 
-			// Process and generate the book
-			log.Printf("Generating Book\n")
-			err = GenerateBook(p, c)
-			if err != nil {
-				fmt.Printf("Error:: %v\n", err)
-				p.Done <- true
-				break
-			}
+	// Process and generate the book
+	log.Printf("Generating Book\n")
+	err = GenerateBook(p, c)
+	if err != nil {
+		fmt.Printf("Error:: %v\n", err)
+		p.Done <- true
+		return err
+	}
 
-			// Just sleep a bit
-			time.Sleep(time.Duration(delay) * time.Millisecond)
-		}
-	}()
-
-	// Wait until we are done
-	<-p.Done
-	// Return from Watch mode
 	return nil
 }
+
+// func WatchMode(delay int64, p *Process) error {
+// 	go func() {
+// 		for true {
+// 			// Get the parts of the config file
+// 			sourcePath := config.SourcePath(source)
+// 			// Set the config file name
+// 			configFile := config.ConfigFile(sourcePath, cfgfile)
+// 			// Log the attempt to read the configuration file
+// 			log.Printf("Reading configuration file %s\n", configFile)
+// 			// Read the configuration
+// 			c, err := config.ReadConfigFromFile(cfgfile, source)
+// 			if err != nil {
+// 				fmt.Printf("Error:: %v\n", err)
+// 				p.Done <- true
+// 				break
+// 			}
+
+// 			// Set the source path
+// 			c.SourcePath = sourcePath
+
+// 			// Process and generate the book
+// 			log.Printf("Generating Book\n")
+// 			err = GenerateBook(p, c)
+// 			if err != nil {
+// 				fmt.Printf("Error:: %v\n", err)
+// 				p.Done <- true
+// 				break
+// 			}
+
+// 			// Just sleep a bit
+// 			time.Sleep(time.Duration(delay) * time.Millisecond)
+// 		}
+// 	}()
+
+// 	// Wait until we are done
+// 	<-p.Done
+// 	// Return from Watch mode
+// 	return nil
+// }
 
 func PrintErr(str string, a ...interface{}) {
 	fmt.Fprintln(os.Stderr, str, a)
